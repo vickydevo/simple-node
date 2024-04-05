4486f46dc2dadc46303f866615539d838fd7e3c0
vignan2020 <88151516+vickydevo@users.noreply.github.com>
2024-04-05 10:06:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function (req, res) {
     if (req.url == '/') {
         res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write('<html><body><p>WELCOME TO UBUNTU Server1111.  This is the page that is visible.</p></body></html>');
+        res.write('<html><body><p>WELCOME TO UBUNTU Server22222.  This is the page that is visible.</p></body></html>');
         
         res.end();
 
